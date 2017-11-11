25fb84a9c67f27bc83fc608cf6a16a4ff197f8c3
qeesung <1245712564@qq.com>
2017-11-11 08:49:35 +0000

    add status check

--- app.py
+++ app.py
@@ -8,6 +8,10 @@ from toilet_config import *
 from toilet_logger import *
 import datetime
 
+STATUS_DOOR_CLOSED="CLOSED"
+STATUS_DOOR_OPEN="OPEN"
+
+toilet_status={}
 
 def initialize_pin(trigger_pin, echo_pin):
     """initialize the io pin"""
@@ -34,18 +38,14 @@ def observe_toilets_status(toilets):
         while True:
             for toilet in toilets:
                 toilet_name=toilet.get(KEY_TOILET_NAME)
-                dis=sample_toilet_distance(toilet)
-                now = datetime.datetime.now()
+                door_distance=round(sample_toilet_distance(toilet), 2)
 
-                print 'Toilet %s distance: %0.2f cm' % (toilet_name, dis)
-
-                log_toilet_status({
-                        KEY_TOILET_ID: toilet.get(KEY_TOILET_ID),
-                        KEY_TOILET_NAME: toilet.get(KEY_TOILET_NAME),
-                        KEY_TOILET_TYPE: toilet.get(KEY_TOILET_TYPE),
-                        "disntance": dis,
-                        "sample_time": str(now)
-                    })
+                print 'Toilet %s distance: %0.2f cm' % (toilet_name, door_distance)
+                if is_toilet_status_changed(toilet, door_distance):
+                    status=get_door_status(door_distance)
+                    update_toilet_status(toilet, status)
+                    print 'Toilet %s status is changed: %s' % (toilet_name, status)
+                log_toilet_status_to_file(toilet, door_distance)
             time.sleep(1)
     except KeyboardInterrupt:
         GPIO.cleanup()
@@ -55,6 +55,57 @@ def sample_toilet_distance(toilet):
     trigger_pin=toilet.get(KEY_TOILET_TRIGGER_PIN)
     echo_pin=toilet.get(KEY_TOILET_ECHO_PIN)
     return check_distance(trigger_pin, echo_pin)
+
+def log_toilet_status_to_file(toilet, door_distance):
+    """log the toilet status to the log file"""
+    now = datetime.datetime.now()
+    log_toilet_status({
+        KEY_TOILET_ID: toilet.get(KEY_TOILET_ID),
+        KEY_TOILET_NAME: toilet.get(KEY_TOILET_NAME),
+        KEY_TOILET_TYPE: toilet.get(KEY_TOILET_TYPE),
+        "disntance": door_distance,
+        "sample_time": str(now)
+    })
+    
+
+def is_toilet_status_changed(toilet, door_distance):
+    """check if the toilet status changed"""
+    current_door_status=get_door_status(door_distance)
+    if not toilet_status.has_key(toilet.get(KEY_TOILET_ID)):
+        update_toilet_status(toilet, current_door_status)
+        return False
+    old_status=toilet_status.get(toilet.get(KEY_TOILET_ID))
+    if current_door_status == old_status:
+        return False
+    for i in xrange(1,2): # recheck the status 2 times
+        temp_distance=round(sample_toilet_distance(toilet), 2)
+        temp_status=get_door_status(temp_distance)
+        if current_door_status != temp_status: # not stable change
+            return False
+    return True
+
+def get_door_status(door_distance):
+    DOOR_CLOSED_DISTANCE_LIMIT=8 # 8cm
+    if door_distance < DOOR_CLOSED_DISTANCE_LIMIT:
+        return STATUS_DOOR_CLOSED
+    else:
+        return STATUS_DOOR_OPEN
+
+def is_door_closed(status):
+    return status == STATUS_DOOR_CLOSED
+
+def is_door_open(status):
+    return status == STATUS_DOOR_OPEN
+
+def update_toilet_status(toilet, status):
+    """"update the toilet status""" 
+    toilet_id=toilet.get(KEY_TOILET_ID)
+    toilet_status[toilet_id]=status
+
+def get_toilet_status(toilet):
+    """get the toilet status"""
+    toilet_id=toilet.get(KEY_TOILET_ID)
+    return toilet_status.get(toilet_id)
 
 initialize_board(TOILETS)     
 observe_toilets_status(TOILETS)
